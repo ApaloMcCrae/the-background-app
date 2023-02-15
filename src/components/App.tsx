c84d73f68eb0f356465ed9183479d8826d8e469d
Peter McCrae <petermchoke@gmail.com>
2023-02-15 10:36:52 -0600

    feat: adding local storage, swatches, inverted close button

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,47 +1,63 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { MantineProvider } from '@mantine/core';
 
 import { AnimatePresence, motion } from 'framer-motion';
 import { ColorPicker } from './ColorPicker';
-import { useIdle } from '@mantine/hooks';
+import { useIdle, useLocalStorage } from '@mantine/hooks';
+import { useWindowHeight } from '../utils/useWindowHeight';
+import { ColorWheelButton } from './ColorWheelButton';
 
 function App() {
-  const [color, setColor] = useState('rgba(47, 119, 150, 0.7)');
+  const [localStorageColor, setLocalStorageColor] = useLocalStorage({
+    key: 'bg-color-value',
+    defaultValue: 'rgba(0, 0, 0, 0.9)',
+  });
+  const [color, setColor] = useState(localStorageColor);
+
   const [showColorPicker, setShowColorPicker] = useState(false);
+  const { windowHeight } = useWindowHeight();
+
+  function handleColorChange(color: string) {
+    setColor(color);
+    setLocalStorageColor(color);
+  }
+
   const idle = useIdle(2000);
+
+  useEffect(() => {
+    if (localStorageColor) {
+      setColor(localStorageColor);
+    }
+  }, [localStorageColor]);
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <div className="relative h-screen w-screen" style={{ background: color }}>
+      <motion.div
+        className="relative h-screen w-screen"
+        style={{ background: color }}
+        initial={{ height: '100vh' }}
+        animate={{ height: windowHeight }}
+        transition={{ duration: 0.3 }}
+      >
         <div className="absolute bottom-4 left-4 text-white flex gap-4 items-center">
           <AnimatePresence mode="wait">
             {showColorPicker ? (
               <ColorPicker
                 color={color}
-                setColor={setColor}
+                setColor={handleColorChange}
                 setShow={setShowColorPicker}
+                key="color-picker"
               />
             ) : !idle ? (
-              <motion.button
-                initial={{ opacity: 0, y: 10 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0 }}
+              <ColorWheelButton
                 onClick={() => setShowColorPicker(true)}
-                className="w-8 h-8 rounded-full border-2 border-white translate"
-                whileTap={{ scale: 0.9 }}
-                whileHover={{
-                  scale: 1.3,
-                  rotate: 45,
-                }}
                 key="color-picker-button"
-              >
-                <img src="/assets/color-wheel.png" alt="color wheel" />
-              </motion.button>
+              />
             ) : (
               <></>
             )}
           </AnimatePresence>
         </div>
-      </div>
+      </motion.div>
     </MantineProvider>
   );
 }
--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,6 +1,7 @@
-import { motion } from 'framer-motion';
-import { DynamicHeroIcon } from './DynamicHeroIcon';
 import { ColorPicker as MantineColorPicker } from '@mantine/core';
+import { motion } from 'framer-motion';
+import invert from 'invert-color';
+import { DynamicHeroIcon } from './DynamicHeroIcon';
 
 export function ColorPicker({
   color,
@@ -21,16 +22,24 @@ export function ColorPicker({
         stiffness: 300,
         damping: 25,
       }}
-      key="color-picker"
     >
-      <MantineColorPicker size="lg" value={color} onChange={setColor} />
+      <MantineColorPicker
+        size="lg"
+        value={color}
+        onChange={setColor}
+        swatches={['#000000', '#ffffff', '#fab005', '#fd7e14']}
+      />
       <motion.button
         whileTap={{ scale: 0.8 }}
         whileHover={{ scale: 1.2 }}
         className="-top-10 -right-10 absolute w-8 h-8"
         onClick={() => setShow(false)}
+        style={{ color: invert(color), backgroundColor: color }}
       >
-        <DynamicHeroIcon icon="XCircleIcon" className="text-gray-300" />
+        <DynamicHeroIcon
+          icon="XCircleIcon"
+          className="drop-shadow-sm rounded-full"
+        />
       </motion.button>
     </motion.div>
   );
