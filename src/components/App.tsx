d6d145c04108b10053f3730b2fe7f930579aafcf
Peter McCrae <petermchoke@gmail.com>
2023-03-06 13:22:05 -0600

    feat: adding custom swatches and context tips

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,67 +1,140 @@
-import { MantineProvider } from "@mantine/core";
-import { useEffect, useState } from "react";
+import { MantineProvider, Kbd } from "@mantine/core";
+import { useState } from "react";
 
-import { useIdle, useLocalStorage, useHotkeys } from "@mantine/hooks";
+import {
+  HotkeyItem,
+  useHotkeys,
+  useIdle,
+  useLocalStorage,
+} from "@mantine/hooks";
+import { Analytics } from "@vercel/analytics/react";
 import { AnimatePresence, motion } from "framer-motion";
 import { useWindowHeight } from "../utils/useWindowHeight";
 import { ColorPicker } from "./ColorPicker";
 import { ColorWheelButton } from "./ColorWheelButton";
-import { Analytics } from "@vercel/analytics/react";
+import { Notifications, notifications } from "@mantine/notifications";
+export interface SwatchType {
+  color: string;
+  keyboardShortcut: string;
+}
 
 function App() {
+  //State
   const [localStorageColor, setLocalStorageColor] = useLocalStorage({
     key: "bg-color-value",
     defaultValue: "#000000",
   });
-  const [color, setColor] = useState(localStorageColor);
-
+  const [swatches, setSwatches] = useLocalStorage<SwatchType[]>({
+    key: "swatches",
+    defaultValue: [
+      { color: "#000000", keyboardShortcut: "1" },
+      { color: "#ffffff", keyboardShortcut: "2" },
+      { color: "", keyboardShortcut: "3" },
+      { color: "", keyboardShortcut: "4" },
+      { color: "", keyboardShortcut: "5" },
+    ],
+  });
   const [showColorPicker, setShowColorPicker] = useState(false);
+  const [hoveringColorWheel, setHoveringColorWheel] = useState(false);
+
+  //Hooks
   const { windowHeight } = useWindowHeight();
-
-  function handleColorChange(color: string) {
-    setColor(color);
-    setLocalStorageColor(color);
-  }
-
   const idle = useIdle(2000);
 
-  useEffect(() => {
-    if (localStorageColor) {
-      setColor(localStorageColor);
+  //Handlers
+  function handleColorChange(color: string) {
+    //Filter out any empty strings
+    if (color) {
+      setLocalStorageColor(color);
+      setHoveringColorWheel(false);
     }
-  }, [localStorageColor]);
+  }
+
+  function handleAddColor(color: string, index: number) {
+    // Check21 that the color isn't already saved
+    const colorAlreadySaved = swatches.some((swatch) => swatch.color === color);
+    if (colorAlreadySaved) {
+      notifications.show({
+        message: "👈 This color is already saved, ya goofball!",
+        autoClose: 3000,
+      });
+      return;
+    }
+    setSwatches((prevSwatches) => {
+      const newSwatches = [...prevSwatches];
+      newSwatches[index].color = color;
+      return newSwatches;
+    });
+  }
+
+  function handleDeleteColor(index: number) {
+    console.log("Deleting index", index);
+    setSwatches((prevSwatches) => {
+      const newSwatches = [...prevSwatches];
+      newSwatches[index].color = "";
+      return newSwatches;
+    });
+  }
+
+  //Spread the swatches into an array of arrays, so we can use the keyboard shortcuts
+  const swatchArray: HotkeyItem[] = swatches.map((swatch) => [
+    swatch.keyboardShortcut,
+    () => handleColorChange(swatch.color),
+  ]);
 
   useHotkeys([
-    ["B", () => handleColorChange("#000000")],
-    ["W", () => handleColorChange("#ffffff")],
-    ["Y", () => handleColorChange("#ffc500")],
+    ...swatchArray,
     ["Escape", () => setShowColorPicker(false)],
     ["Space", () => setShowColorPicker(!showColorPicker)],
   ]);
 
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
+    <MantineProvider
+      // theme={{ colorScheme: "dark" }}
+      withGlobalStyles
+      withNormalizeCSS
+    >
+      <Notifications />
       <motion.div
         className="relative h-screen w-screen"
-        style={{ background: color }}
+        style={{ background: localStorageColor }}
         initial={{ height: "100vh" }}
         animate={{ height: windowHeight }}
-        transition={{ duration: 0.3 }}
+        transition={{ duration: 0.01 }}
       >
         <div className="absolute bottom-4 left-4 text-white flex gap-4 items-center">
           <AnimatePresence mode="wait">
             {showColorPicker ? (
               <ColorPicker
-                color={color}
+                color={localStorageColor}
                 setColor={handleColorChange}
                 setShow={setShowColorPicker}
                 key="color-picker"
+                swatches={swatches}
+                handleAddColor={handleAddColor}
+                handleDeleteColor={handleDeleteColor}
               />
-            ) : !idle ? (
-              <ColorWheelButton
-                onClick={() => setShowColorPicker(true)}
-                key="color-picker-button"
-              />
+            ) : // Show the color wheel button when user is not idle
+            !idle ? (
+              <motion.div
+                onHoverStart={() => setHoveringColorWheel(true)}
+                onHoverEnd={() => setHoveringColorWheel(false)}
+                className="flex items-center gap-4"
+              >
+                <ColorWheelButton
+                  onClick={() => setShowColorPicker(true)}
+                  key="color-picker-button"
+                />
+                {hoveringColorWheel && (
+                  <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                  >
+                    <Kbd>space</Kbd>
+                  </motion.div>
+                )}
+              </motion.div>
             ) : (
               <></>
             )}
--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,47 +1,137 @@
-import { ColorPicker as MantineColorPicker } from '@mantine/core';
-import { motion } from 'framer-motion';
-import invert from 'invert-color';
-import { DynamicHeroIcon } from './DynamicHeroIcon';
+import { ColorPicker as MantineColorPicker, Kbd } from "@mantine/core";
+import { motion } from "framer-motion";
+import invert from "invert-color";
+import { SwatchType } from "./App";
+import { DynamicHeroIcon } from "./DynamicHeroIcon";
+import "./ColorPicker.css";
+import { useState } from "react";
 
 export function ColorPicker({
   color,
   setColor,
   setShow,
+  swatches,
+  handleAddColor,
+  handleDeleteColor,
 }: {
   color: string;
   setColor: (color: string) => void;
   setShow: (show: boolean) => void;
+  swatches: SwatchType[];
+  handleAddColor: (color: string, index: number) => void;
+  handleDeleteColor: (index: number) => void;
 }) {
+  const [showDeleteOnSwatchIndex, setShowDeleteOnSwatchIndex] = useState(-1);
+  const [hoveringEscape, setHoveringEscape] = useState(false);
   return (
     <motion.div
       initial={{ opacity: 0, y: 40 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -10 }}
       transition={{
-        type: 'spring',
+        type: "spring",
         stiffness: 300,
         damping: 25,
       }}
+      layout
     >
-      <MantineColorPicker
-        size="lg"
-        value={color}
-        onChange={setColor}
-        swatches={['#000000', '#ffffff', '#fab005', '#fd7e14']}
-      />
-      {/* Note: We have to use HEX colors, for this invert function to work */}
-      <motion.button
-        whileTap={{ scale: 0.8 }}
-        whileHover={{ scale: 1.2 }}
-        className="-top-10 -right-10 absolute w-8 h-8"
-        onClick={() => setShow(false)}
-        style={{ color: invert(color), backgroundColor: color }}
-      >
-        <DynamicHeroIcon
-          icon="XCircleIcon"
-          className="drop-shadow-sm rounded-full"
-        />
-      </motion.button>
+      <MantineColorPicker size="lg" value={color} onChange={setColor} />
+      <div className="flex gap-x-3 mt-4">
+        {/* Note: We have to use HEX colors, for this invert function to work */}
+        {swatches.map(({ color: swatchColor, keyboardShortcut }, index) => (
+          <motion.div key={index}>
+            {swatchColor ? (
+              <div className="relative">
+                <motion.button
+                  style={{
+                    backgroundColor: swatchColor,
+                    color: invert(swatchColor),
+                  }}
+                  className="p-2 font-extrabold text-2xl rounded-lg shadow-lg w-12 h-12 flex items-center justify-center border-2 border-black/10"
+                  whileHover={{ scale: 1.2 }}
+                  whileTap={{ scale: 0.9 }}
+                  onClick={() => setColor(swatchColor)}
+                  onHoverStart={() => setShowDeleteOnSwatchIndex(index)}
+                  onHoverEnd={() => setShowDeleteOnSwatchIndex(-1)}
+                >
+                  <span className="saturate-0 opacity-75 font-rounded letter-stroke">
+                    {keyboardShortcut}
+                  </span>
+                </motion.button>
+                {index > 1 && index === showDeleteOnSwatchIndex && (
+                  <motion.button
+                    whileHover={{ scale: 1.2 }}
+                    whileTap={{ scale: 0.9 }}
+                    initial={{ scale: 0.8 }}
+                    animate={{ scale: 1 }}
+                    exit={{ scale: 0.8 }}
+                    className="-top-3 -left-2 absolute w-6 h-6 rounded-full"
+                    onClick={() => handleDeleteColor(index)}
+                    style={{ color: invert(color), backgroundColor: color }}
+                    onHoverStart={() => setShowDeleteOnSwatchIndex(index)}
+                    onHoverEnd={() => setShowDeleteOnSwatchIndex(-1)}
+                  >
+                    <DynamicHeroIcon
+                      icon="XCircleIcon"
+                      className="drop-shadow-sm rounded-full saturate-0 contrast-200"
+                    />
+                  </motion.button>
+                )}
+                {/* Little bar to indicate the current color */}
+                <div className="h-2 w-full">
+                  {swatchColor === color && (
+                    <motion.div
+                      className="w-2/4 h-2 rounded-full mt-2 mx-auto saturate-0 contrast-200"
+                      style={{ backgroundColor: invert(color) }}
+                      layoutId="color-picker-indicator"
+                    />
+                  )}
+                </div>
+              </div>
+            ) : (
+              <motion.button
+                className="p-2 font-bold text-2xl rounded-lg shadow-lg w-12 h-12 flex items-center justify-center border-2 border-black/10"
+                style={{
+                  color: invert(color),
+                }}
+                whileHover={{ scale: 1.2 }}
+                whileTap={{ scale: 0.9 }}
+                onClick={() => handleAddColor(color, index)}
+              >
+                <DynamicHeroIcon
+                  icon="PlusIcon"
+                  className="drop-shadow-sm rounded-full saturate-0 contrast-200"
+                />
+              </motion.button>
+            )}
+          </motion.div>
+        ))}
+      </div>
+      <div className="-top-10 -right-10 absolute flex gap-y-2 flex-col">
+        <motion.button
+          whileTap={{ scale: 0.8 }}
+          whileHover={{ scale: 1.2 }}
+          className=" w-8 h-8"
+          onClick={() => setShow(false)}
+          style={{ color: invert(color), backgroundColor: color }}
+          onHoverStart={() => setHoveringEscape(true)}
+          onHoverEnd={() => setHoveringEscape(false)}
+        >
+          <DynamicHeroIcon
+            icon="XCircleIcon"
+            className="drop-shadow-sm rounded-full saturate-0 contrast-200"
+          />
+        </motion.button>
+        {hoveringEscape && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <Kbd>esc</Kbd>
+          </motion.div>
+        )}
+      </div>
     </motion.div>
   );
 }
